09f54722fa34ede1daa846962b5249053076d89e
BaronLip <baronlip@gmail.com>
2019-09-28 23:10:35 -0500

    Update
    Added match_helper methods:
    def match_scores,
    def match_status.

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,7 +1,5 @@
 module GamesHelper
 
-    def game_score
-        self.player_score.to_s + ":" + self.opponent_score.to_s
-    end
+
 
 end
--- app/helpers/matches_helper.rb
+++ app/helpers/matches_helper.rb
@@ -1,36 +1,48 @@
 module MatchesHelper
-
+    
+    
     def player_id_field(match)
         if match.player.nil?
             select_tag "match[player_id]", options_from_collection_for_select(Player.all, :id, :username, match.player_id) 
         else
-			 # Make sure to have the f.hidden_field. This carries the "id" from posts#new over and assigns it to posts#create.
+            # Make sure to have the f.hidden_field. This carries the "id" from posts#new over and assigns it to posts#create.
             hidden_field_tag "match[player_id]", match.player_id
         end
     end
-
+    
     def opponent_id_field(match)
         select_tag "match[opponent_id]", options_from_collection_for_select(Opponent.all, :id, :username)       
     end
-
-    # def games_per_match(match)
-        
-    # end
+    
+    def match_scores(match)
+        @match_scores = []
+        match.games.each do |g|
+            @match_scores.push(g.player_score.to_s + ":" + g.opponent_score.to_s)
+        end
+        @match_scores.join(", ")
+    end
     
     def match_status(match)
-    #     if m.user_score.to_i > m.opponent_score.to_i
-    #         Win
-    #     elsif
-    #         m.user_score.to_i < m.opponent_score.to_i
-    #         Lose
-    #     end
-    end
+        player_wins = 0
+        opponent_wins = 0
+        match.games.each do |g|
+            if g.player_score > g.opponent_score
+                player_wins += 1
+            else
+                opponent_wins += 1
+            end
+        end
 
-    def match_scores(match)
-        # @match_scores = []
-        # match.games.each do |g|
-        #     @match_scores.push(g.game_score)
-        # end
-        # @match_scores
+        if player_wins > opponent_wins
+            "Won"
+        else
+            "Lost"
+        end
     end
+    
 end
+
+# def game_score
+#     # self.player_score.to_s + ":" + self.opponent_score.to_s
+#     "#{self.player_score} : #{self.opponent_score}"
+# end
