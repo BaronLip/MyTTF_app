3f8d878f10e048a854a2b5b2b071a40b1dad71b9
BaronLip <baronlip@gmail.com>
2019-10-03 15:53:31 -0500

    Update
    Added scope methods to Opponent.

--- app/models/opponent.rb
+++ app/models/opponent.rb
@@ -4,4 +4,10 @@ class Opponent < ApplicationRecord
 
     validates_presence_of :username, :gender, :ranking
     validates :username, uniqueness: true
+
+    scope :pro, -> {where("ranking >= 2300")}
+    # scope :elite, -> {where("ranking.between?(1900, 2299)")}
+    scope :elite, -> {where("ranking >= 1900 AND ranking <= 2299")}
+    scope :competitor, -> {where("ranking >= 1400 AND ranking <= 1899")}
+    scope :amateur, -> {where("ranking <= 1399")}
 end
